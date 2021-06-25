eb5136524da20e87d0dcf443656d17a316d54687
Anthony Williams <anthony.williams@unifyconsulting.com>
2021-06-25 16:01:41 -0600

    Add AppBar to top of App (for later nav)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import { Button, Container, makeStyles } from '@material-ui/core'
+import {
+	AppBar,
+	Button,
+	Grid,
+	IconButton,
+	makeStyles,
+	Theme,
+	Toolbar,
+	Typography,
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
 import React, { FC } from 'react'
+import { theme } from './theme'
 import './App.css'
 
 const useStyles = makeStyles({
-	app: {},
+	app: (theme: Theme) => ({
+		marginTop: theme.spacing(2),
+	}),
+	menuButton: (theme: Theme) => ({
+		marginRight: theme.spacing(2),
+	}),
+	title: {
+		display: 'flex',
+		flexGrow: 1,
+	},
 })
 
 const App: FC = () => {
-	const classes = useStyles()
+	const classes = useStyles(theme)
 
 	return (
-		<Container className={classes.app}>
-			<Button color="primary" variant="contained" fullWidth>
-				Button A
-			</Button>
-			<Button color="secondary" variant="contained" fullWidth>
-				Button B
-			</Button>
-			<Button variant="contained" fullWidth>
-				Button C
-			</Button>
-		</Container>
+		<React.Fragment>
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton
+						aria-label="menu"
+						className={classes.menuButton}
+						color="inherit"
+						edge="start"
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" className={classes.title}>
+						News
+					</Typography>
+					<Button color="inherit">Login</Button>
+				</Toolbar>
+			</AppBar>
+			<Grid container className={classes.app}>
+				<Grid item xs={2} />
+				<Grid item container xs={8} spacing={1}>
+					<Grid item xs={12}>
+						<Button color="primary" variant="contained" fullWidth>
+							Button A
+						</Button>
+					</Grid>
+					<Grid item xs={12}>
+						<Button color="secondary" variant="contained" fullWidth>
+							Button B
+						</Button>
+					</Grid>
+					<Grid item xs={12}>
+						<Button variant="contained" fullWidth>
+							Button C
+						</Button>
+					</Grid>
+				</Grid>
+				<Grid item xs={2} />
+			</Grid>
+		</React.Fragment>
 	)
 }
 
