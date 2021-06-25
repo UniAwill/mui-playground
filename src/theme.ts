6f1a3aa689357d33dbd79cfe1dbbf08efd35bcaf
Anthony Williams <anthony.williams@unifyconsulting.com>
2021-06-25 16:03:37 -0600

    Update theme to black and purple

--- src/theme.ts
+++ src/theme.ts
@@ -1,16 +1,12 @@
 import { createMuiTheme } from '@material-ui/core'
-import { blue, blueGrey, grey, red } from '@material-ui/core/colors'
+import { blue, blueGrey, grey, purple, red } from '@material-ui/core/colors'
 
 const theme = createMuiTheme({
 	palette: {
-		// !!! FIXME - background does not update automatically?
 		background: {
-			// default: blueGrey.A400,
-			// default: red.A400,
-			// default: grey.A400,
-			// paper: red.A400,
+			default: grey.A400,
 		},
-		primary: blueGrey,
+		primary: purple,
 		secondary: blue,
 	},
 })
